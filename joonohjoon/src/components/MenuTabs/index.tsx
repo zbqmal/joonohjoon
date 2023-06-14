0e3e0f46335ac04aaa73aef4c968a1568b14c89a
Joonyeol Oh <zbqmal@gmail.com>
2023-06-13 23:23:07 -0400

    feature(TIM-001): menu tabs redirecting to corresponding pages

--- joonohjoon/src/components/MenuTabs/index.tsx
+++ joonohjoon/src/components/MenuTabs/index.tsx
@@ -1,12 +1,15 @@
 import { Tab, Tabs } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const MenuTabs = () => {
+  const router = useRouter();
+
   return (
     <Tabs orientation="vertical">
-      <Tab label="Intro" />
-      <Tab label="Coding" />
-      <Tab label="Astronomy" />
-      <Tab label="Football" />
+      <Tab label="Intro " onClick={() => router.push("/Intro")} />
+      <Tab label="Coding" onClick={() => router.push("/Coding")} />
+      <Tab label="Astronomy" onClick={() => router.push("/Astronomy")} />
+      <Tab label="Football" onClick={() => router.push("/Football")} />
     </Tabs>
   );
 };
